3353b0034faff17de5e7b04f5b656e332a23e207
chris ho <98445729+ho0527@users.noreply.github.com>
2023-10-01 21:47:05 +0800

    new: 20231001

--- extension.js
+++ extension.js
@@ -3,19 +3,37 @@ import vscode from "vscode"
 export default function main(context){
     context.subscriptions.push(vscode.commands.registerTextEditorCommand("extension.formatcode",function(){
         let editor=vscode.window.activeTextEditor // Get active text editor
+        let lang=vscode.window.activeTextEditor.document.languageId
 
         if(editor){
             // Get full content of editor
-            let document=editor.document
-            let content=document.getText()
+            let content=editor.document.getText()
 
             // Format content by removing extra whitespaces
-            let formatted=content.replace(/\s+/g,"")
+            let formatted=content.replace(/\s+/gi,"")
 
             // Update document content
             editor.edit(function(event){
-                event.replace(new vscode.Range(0,0,document.lineCount,0),formatted)
+                event.replace(new vscode.Range(0,0,editor.document.lineCount,0),formatted)
             })
+
+            if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else if(lang=="html"){
+            }else{
+            }
         }else{ return }
     }))
 }
